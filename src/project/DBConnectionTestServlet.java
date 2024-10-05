bc6c72e836f608503356181357295aa79d497341
KASUNHapangama <88395585+KASUNHapangama@users.noreply.github.com>
2024-10-05 14:04:12 +0530

    [change] added 404 error page

--- src/project/DBConnectionTestServlet.java
+++ src/project/DBConnectionTestServlet.java
@@ -3,7 +3,6 @@ package project;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.naming.Context;
 import javax.naming.InitialContext;
