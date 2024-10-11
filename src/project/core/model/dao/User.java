48b458865ec274b8e3d18ae6f94ca98ab64faff8
Ishini <ishinineha@gmail.com>
2024-10-11 23:09:53 +0530

    Update User.java

--- src/project/core/model/dao/User.java
+++ src/project/core/model/dao/User.java
@@ -27,6 +27,59 @@ public class User {
 		this.password = password;
 	}
 
+	public User(int id, String userName, String email, String password, String role) {
+		super();
+		this.id = id;
+		this.userName = userName;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+	}
+
+	public User() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
 	
 
 }
